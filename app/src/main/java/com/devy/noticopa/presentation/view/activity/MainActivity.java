7850a18a70d6a31c18b010c2d25bf7b150be16d5
Ronald Velasquez <ronaldvg93@gmail.com>
2016-06-25 17:20:39 -0500

    First commit

---
+++ app/src/main/java/com/devy/noticopa/presentation/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.devy.noticopa.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.devy.noticopa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
